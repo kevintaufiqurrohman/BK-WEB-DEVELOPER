c3161590c12d668172f4d76b10f6f7c57b4ff4e4
kevintaufiqurrohman <132144413+kevintaufiqurrohman@users.noreply.github.com>
2024-01-05 20:46:43 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'poli2';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
+
+?>
